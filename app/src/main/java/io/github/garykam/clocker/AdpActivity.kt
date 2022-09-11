0ef21a291b91c1b69c64b177597f96424a260a4d
Gary Kam <garyhkam@gmail.com>
2022-09-10 22:02:43 -0700

    login to adp with user credentials

--- app/src/main/java/io/github/garykam/clocker/AdpActivity.kt
+++ app/src/main/java/io/github/garykam/clocker/AdpActivity.kt
@@ -2,6 +2,9 @@ package io.github.garykam.clocker
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.KeyCharacterMap
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
@@ -25,31 +28,82 @@ class AdpActivity : ComponentActivity() {
     private fun AdpView() {
         AndroidView(factory = {
             WebView(this).apply {
-                webViewClient = object : WebViewClient() {
-                    override fun onPageFinished(view: WebView?, url: String?) {
-                        super.onPageFinished(view, url)
-                        loginToAdp(view)
-                    }
-                }
+                var page = 0
 
                 loadUrl("https://login.adp.com/welcome")
                 settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+
+                webViewClient = object : WebViewClient() {
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        if (page == 0) {
+                            page++
+                            visitLoginPage(view)
+                        } else if (page == 1) {
+                            page++
+                            loginWithUserCredentials(view)
+                        }
+                    }
+                }
             }
         }, modifier = Modifier.fillMaxSize())
     }
 
-    private fun loginToAdp(webView: WebView?) {
+    private fun visitLoginPage(webView: WebView?) {
         webView?.loadUrl(
-            "javascript:(function() {                                                                " +
-                    "clickEvent = document.createEvent('HTMLEvents');                                " +
-                    "clickEvent.initEvent('click', true, true);                                      " +
-                    "dropdown = document.querySelectorAll('div[class=\"select__trigger\"]')[0];      " +
-                    "option = document.querySelectorAll('span[data-value=\"anActiveEmployee\"]')[0]; " +
-                    "nextButton = document.querySelector('button[id=\"next\"]');                     " +
-                    "dropdown.dispatchEvent(clickEvent);                                             " +
-                    "option.dispatchEvent(clickEvent);                                               " +
-                    "nextButton.dispatchEvent(clickEvent);                                           " +
-                    "}) ()                                                                           "
+            "javascript: (function() {                                                         " +
+                    "clickEvent = document.createEvent('HTMLEvents');                          " +
+                    "clickEvent.initEvent('click', true, true);                                " +
+                    "dropdown = document.querySelector('div[class=\"select__trigger\"]');      " +
+                    "option = document.querySelector('span[data-value=\"anActiveEmployee\"]'); " +
+                    "nextButton = document.querySelector('button[id=\"next\"]');               " +
+                    "dropdown.dispatchEvent(clickEvent);                                       " +
+                    "option.dispatchEvent(clickEvent);                                         " +
+                    "nextButton.dispatchEvent(clickEvent);                                     " +
+                    "}) ()                                                                     "
         )
+    }
+
+    private fun loginWithUserCredentials(webView: WebView?) {
+        val username = "username"
+        val password = "password"
+        val keyMap = KeyCharacterMap.load(KeyCharacterMap.VIRTUAL_KEYBOARD)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            keyMap.getEvents(username.toCharArray()).forEach {
+                webView?.dispatchKeyEvent(it)
+            }
+
+            webView?.requestFocus()
+            webView?.loadUrl(
+                "javascript: (function() {                                       " +
+                        "clickEvent = document.createEvent('HTMLEvents');        " +
+                        "clickEvent.initEvent('click', true, true);              " +
+                        "nextButton = document.getElementById('verifUseridBtn'); " +
+                        "nextButton.dispatchEvent(clickEvent);                   " +
+                        "}) ()"
+            )
+        }, DELAY)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            keyMap.getEvents(password.toCharArray()).forEach {
+                webView?.dispatchKeyEvent(it)
+            }
+        }, DELAY * 3)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            webView?.loadUrl(
+                "javascript: (function() {                                  " +
+                        "clickEvent = document.createEvent('HTMLEvents');   " +
+                        "clickEvent.initEvent('click', true, true);         " +
+                        "signInButton = document.getElementById('signBtn'); " +
+                        "signInButton.dispatchEvent(clickEvent);            " +
+                        "}) ()"
+            )
+        }, DELAY * 4)
+    }
+
+    companion object {
+        private const val DELAY = 500L
     }
 }
